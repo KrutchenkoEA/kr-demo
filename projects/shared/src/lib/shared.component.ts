f834e9240e9ce6a9d40340f0e9986499789282c9
Евгений Крютченко <kryutchenkoe@terralink-global.com>
2024-10-14 10:52:43 +0400

    update dependencies

---
+++ projects/shared/src/lib/shared.component.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lib-shared',
+  standalone: true,
+  imports: [],
+  template: ` <p>shared works!</p> `,
+  styles: ``,
+})
+export class SharedComponent {}
