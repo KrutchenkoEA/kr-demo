1ebb065e944e33b5a51231d4c76527a9233e98fc
Евгений Крютченко <kryutchenkoe@terralink-global.com>
2025-01-09 16:57:08 +0400

    fix tsconfig path

--- projects/kr-app/src/app/pages/charts/charts.component.ts
+++ projects/kr-app/src/app/pages/charts/charts.component.ts
@@ -1,13 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { KruiChartComboModule, kruiChartNumberMock3, kruiChartNumberMock4 } from '@kr-platform/ui';
 
 @Component({
   selector: 'kr-app-charts',
-  imports: [],
+  imports: [
+    KruiChartComboModule,
+  ],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.scss',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartsComponent {
-
+  public data = kruiChartNumberMock3;
+  public data2 = kruiChartNumberMock4;
 }
