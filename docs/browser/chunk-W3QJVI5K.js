c9c4eb50e8a4fc50e9e004b23034e4fd254cb3cc
Евгений Крютченко <kryutchenkoe@terralink-global.com>
2025-02-21 18:51:01 +0400

    set workflow

---
+++ docs/browser/chunk-W3QJVI5K.js
@@ -0,0 +1,1 @@
+var T="cubic-bezier(0, 0, .2, 1)",N="cubic-bezier(0.25, 0.8, 0.25, 1)",I=`180ms ${T}`,o=`700ms ${T}`,t=`300ms ${T}`,s=`400ms ${N}`,O=`400ms ${N}`;export{I as a,t as b,s as c,O as d};
