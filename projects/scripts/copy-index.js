304a349200e2dfbb8df6a478d87694b65c8a2e5d
Евгений Крютченко <kryutchenkoe@terralink-global.com>
2025-02-24 13:09:15 +0400

    fix deploy config

--- projects/scripts/copy-index.js
+++ projects/scripts/copy-index.js
@@ -1,5 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-const indexDir = path.join(__dirname, "/../../docs/browser/index.html");
-const pageDir = path.join(__dirname, "/../../docs/browser/404.html");
+const indexDir = path.join(__dirname, "/../../docs/index.html");
+const pageDir = path.join(__dirname, "/../../docs/404.html");
 fs.copyFileSync(indexDir, pageDir);
